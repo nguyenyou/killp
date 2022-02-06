1d7aed77cd6a5ea33e6a820be7919a4cd06ff77c
nguyenyou <tunt081295@gmail.com>
2022-02-06 22:56:22 +0700

    next

--- index.mjs
+++ index.mjs
@@ -4,12 +4,12 @@
 import ora from "ora";
 import chalk from "chalk";
 import gradient from "gradient-string";
+import pidFromPort from "pid-from-port";
 import { execSync } from "child_process";
+import os from "os";
 
-const ports = process.argv;
-ports.shift();
-ports.shift();
-const turboGradient = gradient("#0099F7", "#F11712");
+const ports = process.argv.slice(2);
+const turboGradient = gradient("#0099F7", "#F11712"); // from Turborepo project
 
 const THANK_YOU = "Life is full of choices. Thanks for choosing me!";
 
@@ -21,9 +21,16 @@ async function main() {
   const spinner = ora(`Killing: ${list}`).start();
 
   try {
-    ports.forEach((port) =>
-      execSync(`lsof -i:${port} | xargs killall`, { stdio: "ignore" })
-    );
+    if (os.platform() === "win32") {
+      for(let i = 0; i < ports.length; i++) {
+        const pid = await pidFromPort(port);
+        execSync(`taskkill /pid ${pid} /f`);
+      }
+    } else {
+      ports.forEach((port) =>
+        execSync(`lsof -i:${port} | xargs killall`, { stdio: "ignore" })
+      );
+    }
     spinner.succeed(`Goodbye: ${list}`);
     console.log(chalk.bold(turboGradient(`\n>>> ${THANK_YOU}\n`)));
   } catch {
