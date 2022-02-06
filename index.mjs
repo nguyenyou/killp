f9604a69cd698be28ed7ee5404a7da1bbbad602c
You Nguyen <tunt081295@gmail.com>
2022-02-07 01:38:58 +0700

    next

--- index.mjs
+++ index.mjs
@@ -14,6 +14,7 @@ const turboGradient = gradient("#0099F7", "#F11712") // from Turborepo project
 const THANK_YOU = "Life is full of choices. Thanks for choosing me!"
 
 async function main() {
+  const killed = []
   if (!Array.isArray(ports) || ports.length === 0) return
   console.log(``)
 
@@ -23,17 +24,27 @@ async function main() {
   try {
     if (os.platform() === "win32") {
       for (let i = 0; i < ports.length; i++) {
-        const pid = await pidFromPort(Number(ports[i]))
+        const port = ports[i]
+        const pid = await pidFromPort(Number(port))
         execSync(`taskkill /pid ${pid} /f`)
+        killed.push(port)
       }
     } else {
       const command = (port) => `lsof -i :${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`
-      ports.forEach((port) => execSync(command(port), { stdio: "ignore" }))
+      ports.forEach((port) => {
+        execSync(command(port), { stdio: "ignore" })
+        killed.push(port)
+      })
     }
     spinner.succeed(`Goodbye: ${list}`)
     console.log(chalk.bold(turboGradient(`\n>>> ${THANK_YOU}\n`)))
   } catch {
-    spinner.fail("No matching processes belonging to you were found. Nothing is killed.\n")
+    if (killed.length === 0) {
+      spinner.fail("No matching processes belonging to you were found. Nothing is killed.\n")
+    } else {
+      const killedList = killed.map((p) => chalk.hex("FE9900")(p)).join(", ")
+      spinner.succeed(`Goodbye: ${killedList}`)
+    }
   }
 }
 
